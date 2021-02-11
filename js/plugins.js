ec160a1026eebb73e098ee740c220ef907090bdf
Matteo Pelosi <matteo.pelosi@me.com>
2021-02-11 14:59:24 -0600

    Add Countdown

--- js/plugins.js
+++ js/plugins.js
@@ -21,4 +21,36 @@
   }
 }());
 
-// Place any jQuery/helper plugins in here.
+// Countdown
+
+// Set the date we're counting down to
+var countDownDate = new Date("Feb 22, 2021 12:00:00").getTime();
+
+
+function countDown(){
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="demo"
+  document.getElementById("countdown").innerHTML = days + ":" + hours + ":"
+      + minutes + ":" + seconds + "";
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "";
+  }
+
+  setTimeout(countDown, 1000);
+}
+
+countDown();
